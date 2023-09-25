4a289f0966a8128023223a586180b0231736770a
salman608 <salman608bd@gmail.com>
2023-09-25 21:19:40 +0600

    admin logout added

--- routes/web.php
+++ routes/web.php
@@ -34,4 +34,5 @@ require __DIR__.'/auth.php';
 //Admin Group Route
 Route::middleware(['auth','roles:admin'])->group(function(){
     Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
+    Route::get('/admin/logout', [AdminController::class, 'AdminLogout'])->name('admin.logout');
 });
